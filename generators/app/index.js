be5ec143f35aa7e63a0555a786727378582351b1
alestor123 <alestoraldous@gmail.com>
2021-08-15 11:26:13 +0000

    changed json

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,16 +1,16 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+'use strict'
+const Generator = require('yeoman-generator')
+const chalk = require('chalk')
+const yosay = require('yosay')
 
 module.exports = class extends Generator {
-  prompting() {
+  prompting () {
     // Have Yeoman greet the user.
     this.log(
       yosay(
         `Welcome to the impeccable ${chalk.red('generator-multi')} generator!`
       )
-    );
+    )
 
     const prompts = [
       {
@@ -19,22 +19,22 @@ module.exports = class extends Generator {
         message: 'Would you like to enable this option?',
         default: true
       }
-    ];
+    ]
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
+      this.props = props
+    })
   }
 
-  writing() {
+  writing () {
     this.fs.copy(
       this.templatePath('dummyfile.txt'),
       this.destinationPath('dummyfile.txt')
-    );
+    )
   }
 
-  install() {
-    this.installDependencies();
+  install () {
+    this.installDependencies()
   }
-};
+}
